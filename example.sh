c572e5d1be9479a0bd2480885ecfd0ed1ed57da0
David D <david.domask@gmail.com>
2018-03-10 23:33:45 +0300

    second commit

--- example.sh
+++ example.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 clear
 echo "Hi Ruslan!"
+echo "Are you enjoying learning so far?"
+options=("Yes" "No" "I'm Lost")
+echo "Enter a number"
+select opt in "${options[@]}" "Quit"; do
+	case $opt in 
+		"Yes")
+			echo "Great! Keep it up!"
+			;;
+		"No")
+			echo "Oh no! Don't worry, keep trying!"
+			;;
+		"I'm Lost")
+			echo "We all are..."
+			;;
+		"Quit")
+			break
+			;;
+		*) echo invalid option;;
+	esac
+done
